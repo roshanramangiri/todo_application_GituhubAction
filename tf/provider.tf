09feb485cc43003b9fe819b14cc11287a8c72435
roshanramangiri <roshanramangiri@gmail.com>
2023-05-19 20:54:40 +0545

    provider.tf updated

--- tf/provider.tf
+++ tf/provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
-  profile = "default"
-}
+# # Configure the AWS Provider
+# provider "aws" {
+#   region = "us-east-1"
+#   profile = "default"
+# }
